76bcd735b238984948ee94fd3c7c21d4e7ff2f37
Dimitri Chenet <dchenet1017@gmail.com>
2022-01-19 02:01:57 -0500

    commit tree 73414ea8aac4773377986f83ebd87b93f06fd391

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,15 @@
 # your code here
+class MySet 
+    def initialize(enumerable = [])
+        # set = MySet.new
+        # set = MySet.new([1,2,3,3])
+        @hash = {}
+         enumerable.each do |value|
+          @hash[value] = true
+    end
+
+    end
+def include?(value)
+    @hash.has_key?(value)
+end
+end
